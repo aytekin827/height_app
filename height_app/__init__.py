64dd78ee3272d36d1016884181677287e46ea4c0
aytekin <mjeong827@gmail.com>
2021-12-06 19:44:56 +0900

    modifiy database address

--- height_app/__init__.py
+++ height_app/__init__.py
@@ -13,10 +13,17 @@ def create_app():
     """
     app = Flask(__name__)
 
+    # 로컬서버에서 연습할 때
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1234@localhost:5433/worldcup"
+
+    # 헤로쿠에서 만든 postgresql database 주소
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://gqaykyrqekaaqo:fc0ccbec140035513e457fecde738273d4750450e2d47de1a22e85fec18e95ce@ec2-34-225-103-117.compute-1.amazonaws.com:5432/d8uluv6ocm72vm"
-    
-    
+
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app,db)
 
     from height_app.routes import main_route,users_route
     app.register_blueprint(main_route.bp)
